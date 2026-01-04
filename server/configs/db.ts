2087d25ca5597f3052935c9fc6015534e6a94aac
pradyumn8 <96361733+pradyumn8@users.noreply.github.com>
2026-01-04 20:56:55 +0530

    Backend setup with required package & db Connented

---
+++ server/configs/db.ts
@@ -0,0 +1,13 @@
+import mongoose from 'mongoose';
+
+const connectDB = async () => {
+    try {
+        mongoose.connection.on('connected', ()=> console.log('MongoDB connected successfully'));
+        await mongoose.connect(process.env.MONGODB_URI as string);
+    } catch (error) {
+        console.error('MongoDB connection error:', error);
+        process.exit(1);
+    }
+};  
+
+export default connectDB;
