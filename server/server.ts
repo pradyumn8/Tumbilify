6ca52e13f627a8dacf1c23deb29682120bef75ee
pradyumn8 <96361733+pradyumn8@users.noreply.github.com>
2026-01-04 21:13:46 +0530

    installed required package & setup session for 7days

--- server/server.ts
+++ server/server.ts
@@ -2,10 +2,31 @@ import express, { Request, Response } from 'express';
 import cors from 'cors';
 import 'dotenv/config';
 import connectDB from './configs/db.js';
+import session from 'express-session';
 
+
+declare module 'express-session' {
+    interface SessionData {
+        isLoggedIn: boolean;
+        userId: string;
+    }
+}
 await connectDB();
 
 const app = express();
+
+app.use(cors({
+    origin: ['http://localhost:5173', 'http://localhost:3000'],
+    credentials: true
+}));
+
+app.use(session({
+    secret: process.env.SESSION_SECRET as string,
+    resave: false,
+    saveUninitialized: false,
+    cookie: { secure: false, httpOnly: true, maxAge: 1000 * 60 * 60 * 24 * 7 } // 7 days
+    
+}));
 
 app.use(express.json());
 
